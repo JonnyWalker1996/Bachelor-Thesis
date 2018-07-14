df1ef1ea3c21a0dec16b31c6da49b7609c3c6381
Jonathan Beller <jonathanbeller@gmx.de>
2018-07-14 13:42:33 +0200

    Added next steps, modified source files to work when killing ripd daemon

---
+++ src/config.h
@@ -0,0 +1,34 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `/home/osboxes/frr' library
+   (-l/home/osboxes/frr). */
+/* #undef HAVE_LIB_HOME_OSBOXES_FRR */
+
+/* Define to 1 if you have the `/home/osboxes/frr/lib' library
+   (-l/home/osboxes/frr/lib). */
+/* #undef HAVE_LIB_HOME_OSBOXES_FRR_LIB */
+
+/* Name of package */
+#define PACKAGE "mydaemon"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "bug-automake@gnu.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "mydaemon"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "mydaemon 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "mydaemon"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
